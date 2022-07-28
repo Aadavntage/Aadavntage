a8698d4a01cc2e6459d4f096b9f06bf096dfd69c
Aadavntage <96700232+Aadavntage@users.noreply.github.com>
2022-07-27 16:55:39 -0800

    Add files via upload

---
+++ DrawingWithText.java
@@ -0,0 +1,20 @@
+
+public class DrawingWithText {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println ("         __     __");
+		System.out.println ("        /  \\___/  \\ ");
+		System.out.println ("   ,--(      ..    )");
+		System.out.println ("  /    \\ __    __/");
+		System.out.println (" /        ( \\  |(");
+		System.out.println ("^ \\   /--\\  /\\ |");
+		System.out.println ("  |__|   |__| ||");
+
+	}
+
+}
